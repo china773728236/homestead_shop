af8ec7f58bb7856f071bf41e66fc4c3d8398dc81
jiezai <773728236@qq.com>
2019-02-14 08:16:04 +0800

    前端远程模板mix

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+//处理所有自定义页面的逻辑
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //
+    public function root()
+    {
+        return view('pages.root');
+    }
+}
